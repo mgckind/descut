d0d326237fae17c6b7fcbc78ca6480c13a4810d3
Matias Carrasco Kind <mgckind@gmail.com>
2016-08-01 01:47:49 -0500

    main pages

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -9,10 +9,17 @@
 
 
   window.addEventListener('WebComponentsReady', function() {
-    app.selection="0";
+    var pages = document.querySelector('iron-pages');
     var menu = document.querySelector('paper-menu');
+    app.selection="0";
+    pages.selected="0";
+    menu.selected="0";
         menu.addEventListener('iron-select', function() {
             app.selection=menu.selected;
+            pages.selected=menu.selected;
+           app.$.paperDrawerPanel.closeDrawer();
+        });
+        menu.addEventListener('iron-activate', function() {
            app.$.paperDrawerPanel.closeDrawer();
         });
   });
