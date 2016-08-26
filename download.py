2215a7a9c277f85950c6e6cf70faec3ee74dfd51
Weixiang Yu <wyu16@illinois.edu>
2016-08-26 09:34:42 -0500

    change tar location for easier cleanup

--- download.py
+++ download.py
@@ -21,23 +21,24 @@ class DownloadHandler(BaseHandler):
     def post(self):
         pngName = self.get_argument("pngName")
         path = self.get_argument("path")
+        jobid = self.get_argument("jobid")
         # print('+-+-+-+-',pngName)
         # print('+-+-+-+-',path)
         fits_name = pngName.replace('.png','.fits')
         # user_folder=os.path.join(Settings.UPLOADS,self.current_user.replace('\"','')) + '/'
         username = self.current_user.replace('\"', '')
         app_dir = os.path.dirname(__file__)
-        archiveFolder =  os.path.join(app_dir, 'static/uploads', username, 'results/tar/')
-        
         tarName = pngName.replace('.png', '.tar.gz')
         filegz = path.replace('.png', '.tar.gz')
-        if os.path.exists(archiveFolder+tarName):
+        tarPath =  os.path.join(app_dir, 'static/uploads', username, 'results', jobid, tarName)
+    
+        if os.path.exists(tarPath):
             self.set_status(200)
             self.flush()
         else:
             os.chdir(app_dir+os.path.split(path)[0]+'/')
-            print (os.getcwd())
-            subprocess.check_call("tar -zcf {} {} {}".format(archiveFolder+tarName, pngName, fits_name), shell=True) 
+            # print (os.getcwd())
+            subprocess.check_call("tar -zcf {} {} {}".format(tarPath, pngName, fits_name), shell=True) 
             os.chdir(app_dir)
             
             self.set_status(200)
@@ -48,18 +49,19 @@ class DownloadObjectHandler(BaseHandler):
     @tornado.web.authenticated
     def post(self):
         path = self.get_argument("path")
-        jobid_short = self.get_argument("jobid")[:6]
+        siid = self.get_argument("jobid")
+        jobid_short = siid[:6]
         username = self.current_user.replace('\"', '')
         app_dir = os.path.dirname(__file__)
-        archiveFolder =  os.path.join(app_dir, 'static/uploads', username, 'results/tar/')
+        archiveFolder =  os.path.join(app_dir, 'static/uploads', username, 'results', siid)
         tarName = jobid_short+'_'+os.path.split(path)[1]+'.tar.gz'
-
-        if os.path.exists(archiveFolder+tarName):
+        tarPath = os.path.join(archiveFolder,tarName)
+        if os.path.exists(tarPath):
             self.set_status(200)
             self.flush()
         else:
             os.chdir(app_dir+os.path.split(path)[0])
-            subprocess.check_call("tar -zcf {} {}".format(archiveFolder+tarName, os.path.split(path)[1]+'/'), shell=True) 
+            subprocess.check_call("tar -zcf {} {}".format(tarPath, os.path.split(path)[1]+'/'), shell=True) 
             os.chdir(os.path.dirname(__file__))
             
             self.set_status(200)
