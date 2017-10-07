edf271565d20b55da992d72e20da11578ac434e3
Matias Carrasco Kind <mgckind@gmail.com>
2017-10-07 00:11:12 -0500

    version  8274d0f

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """descut version"""
 
-version_tag = (1, 1, 2, 'dev-263d4db')
+version_tag = (1, 1, 2, 'dev- 8274d0f')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
